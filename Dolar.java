5d4f4366520cc6e793ff22f91339fdc33560fa8c
Heloisa Cristina <heloisacristinahc0304@gmail.com>
2025-09-01 10:28:37 -0300

    Add files via upload

---
+++ Dolar.java
@@ -0,0 +1,25 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package dolar;
+import java.util.Scanner;
+
+public class Dolar {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        
+        System.out.println("Digite o valor da taxa de conversao atual do dolar para o real: ");
+        float taxa = sc.nextFloat();
+        
+        System.out.println("Digite quantos dolares voce tem: ");
+        float dolar = sc.nextFloat();
+        
+        float reais = taxa * dolar;
+        
+        System.out.println("O valor dos dolares em reais e: " + reais);
+        
+        sc.close();
+    }
+    
+}
